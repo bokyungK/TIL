af75e467522de99299a69501bb99b2f6d2e3757d
bokyungK <freebob2b@gmail.com>
2022-06-20 22:17:52 +0900

    study about basic type and rules of ts

---
+++ typescript/1_type-basic.ts
@@ -0,0 +1,13 @@
+// 문자열
+const str: string = 'hello';
+
+// 숫자
+const num: number = 10;
+
+// 배열
+// 방법 1
+const arr: Array<number> = [1, 2, 3];
+// 1-1 배열의 요소로 number만 가능
+// 1-2 배열 타입은 '대문자'로 시작
+// 방법 2
+const items: number[] = [1, 2, 3];
