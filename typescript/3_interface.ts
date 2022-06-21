361847bebc951380fdfa93500082c00029313997
bokyungK <freebob2b@gmail.com>
2022-06-21 20:42:18 +0900

    study interface with function

--- typescript/3_interface.ts
+++ typescript/3_interface.ts
@@ -3,7 +3,18 @@ interface User {
     name: string;
 }
 
+// 변수에 인터페이스 활용
 const Liz: User = {
     age: 29,
     name: '보경'
 };
+
+// 함수에 인터페이스 활용
+function getUser(user: User) { // User interface 형식만 받겠다고 정의
+    console.log(getUser);
+}
+const Nina = {
+    age: 33,
+    name: '현진'
+}
+getUser(Nina);
