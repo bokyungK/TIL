f04541cf3f3f25ef1efc80e1878564633a1109dd
bokyungK <freebob2b@gmail.com>
2022-06-21 19:01:33 +0900

    study argument of js

---
+++ typescript/2_functions.js
@@ -0,0 +1,14 @@
+// 1. 함수의 파라미터에 타입을 정의하는 방식
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
+// 2. 함수의 반환 값에 타입을 정의하는 방식
+function add() {
+    return 30;
+}
+// 3. 종합 - 함수에 타입을 정의하는 방식***
+function min(a, b) {
+    return a - b;
+}
+min(3, 1, 0, 4);
