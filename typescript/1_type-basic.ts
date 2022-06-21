d6657b728b958e40e581d5a3f3b454b752909cb6
bokyungK <freebob2b@gmail.com>
2022-06-21 12:48:59 +0900

    study about more basic types and rules of ts

--- typescript/1_type-basic.ts
+++ typescript/1_type-basic.ts
@@ -11,3 +11,17 @@ const arr: Array<number> = [1, 2, 3];
 // 1-2 배열 타입은 '대문자'로 시작
 // 방법 2
 const items: number[] = [1, 2, 3];
+
+// 튜플
+const address: [number, string, boolean] = [1, '하나', true];
+
+// 객체
+// 방법1 (기본 버전)
+const obj: object = {};
+// 방법2 (구체적인 버전)
+const obj2: {name: string, age: number} = {
+    name: 'Liz',
+    age: 29
+}
+
+
