9980e91d984a195f432d61972ee1a839bc33192c
bokyungK <freebob2b@gmail.com>
2022-06-21 13:33:32 +0900

    study optional parameters

--- typescript/2_functions.ts
+++ typescript/2_functions.ts
@@ -13,5 +13,13 @@ function add(): number {
 function min(a: number, b: number): number {
     return a - b;
 }
-
 min(3, 1, 0, 4); // js로 변환 시 초과된 인수에 관한 에러 출력
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b: string, c?:string) {
+
+}
+log('hello'); // 에러 발생
+// 아래 두 경우는 옵셔널 파라미터 때문에 규칙에 위배되지 않음
+log('hello', 'world');
+log('hello', 'world', '!');
