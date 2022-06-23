013afc2fe9a7b5a3e8fb9a5fa0767d31761d584c
bokyungK <freebob2b@gmail.com>
2022-06-23 21:57:35 +0900

    class and constructor function

--- typescript/class.js
+++ typescript/class.js
@@ -1,3 +1,11 @@
+// 1
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+const seho = new Person('세호', 30);
+
+// 2
 class Person {
     constructor(name, age) {
         console.log('생성되었습니다');
@@ -5,6 +13,6 @@ class Person {
         this.age = age;
     }
 }
-
 const seho = new Person('세호', 30);
-console.log(seho); // 생성되었습니다 (줄바꿈) Person ~
+
+// => 1(생성자 함수) 2(클래스)**의 내부적인 로직은 같고, 작성 방식이 다르다.
