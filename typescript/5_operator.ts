5d37b305ab8f8b692172ed83dcf371a957b007e9
bokyungK <freebob2b@gmail.com>
2022-06-23 18:24:28 +0900

    study adventage of using union type

--- typescript/5_operator.ts
+++ typescript/5_operator.ts
@@ -9,7 +9,13 @@ logMessage(100); // eslint error
 
 // 해결 방법2 (타입의 특성을 지키는 방법) => 유니온 타입
 function logMessage(value: string | number) {
-    console.log(value);
+    if (typeof value === 'number') {
+        value.toString(); // 유니온 타입 장점 : 타입 가드
+    }
+    if (typeof value === 'string') {
+        value.toLocaleUpperCase();
+    }
+    throw new TypeError('value must be string or number'); // throw error
 }
 logMessage('hello');
 logMessage(100);
