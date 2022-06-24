db5317959786bc8a046d3f3f3664ab576147864a
bokyungK <freebob2b@gmail.com>
2022-06-24 23:51:53 +0900

    learn advantage of generics

--- typescript/8_generics.ts
+++ typescript/8_generics.ts
@@ -1,6 +1,15 @@
-function logText<T>(text: T):T {
+// function logText<T>(text: T):T {
+//     console.log(text);
+//     return text;
+// }
+// logText('하이'); // text의 타입을 인수에서 전달
+// logText<string>('Hi'); // 명시적으로 타입 전달
+
+function logText<T>(text: T): T {
     console.log(text);
     return text;
 }
-logText('하이'); // text의 타입을 인수에서 전달
-logText<string>('Hi'); // 명시적으로 타입 전달
+
+const str = logText<string>('abc');
+str.split('');
+const login = logText<bolean>(true);
