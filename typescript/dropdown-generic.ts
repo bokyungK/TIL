24e8b292a9c1ce976c57a9fe0dad855c8e95ba36
bokyungK <freebob2b@gmail.com>
2022-07-04 23:10:31 +0900

    refactoring interface codes with generic

--- typescript/dropdown-generic.ts
+++ typescript/dropdown-generic.ts
@@ -1,27 +1,23 @@
-interface Email {
-  value: string;
+interface DropdownItem<T> {
+  value: T;
   selected: boolean;
 }
 
-const emails: Email[] = [
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
 
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
 
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem<T>(item: DropdownItem<T> | DropdownItem<T>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -31,13 +27,13 @@ function createDropdownItem(item: Email | ProductNumber) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
 
 numberOfProducts.forEach(function (product) {
-  const item = createDropdownItem(product);
+  const item = createDropdownItem<number>(product);
 });
 
 // 인터페이스에 제네릭을 선언하는 방법
