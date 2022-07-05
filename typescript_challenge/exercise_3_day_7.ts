9d304ce6cee2292a76528d4a4a74a26a3363d35b
bokyungK <freebob2b@gmail.com>
2022-07-05 23:59:43 +0900

    my first answer

--- typescript_challenge/exercise_3_day_7.ts
+++ typescript_challenge/exercise_3_day_7.ts
@@ -1,19 +1,41 @@
 // 1 - Use abstract classes and generics.
+    // 1 - my answer
 interface LocalStorageAPI<T> {
-
+    [key: string]: T
 }
 
-// localStorage.setItem(<key>, <value>)
-// localStorage.getItem(<key>)
-// localStorage.clearItem(<key>)
-// localStorage.clear()
+abstract class LocalStorageClass<T> {
+    constructor(
+        public localStorage: LocalStorageAPI<T> = {}
+    ) {}
+    public setItem(key: string, value: T): void {
+        this.localStorage[key] = value;
+    }
+    public getItem(key: string) {
+        return this.localStorage[key];
+    }
+    public clearItem(key: string) {
+        delete this.localStorage[key];
+    }
+    public clear() {
+        this.localStorage = {};
+    }
+}
+
+class LocalStorage2<V> extends LocalStorageClass<V> {
+}
+const localStorage2 = new LocalStorage2();
+
+localStorage2.setItem('level', 3);
+localStorage2.getItem('level');
+localStorage2.clearItem('level');
+localStorage2.clear();
 
 
 // 2 - Use overloading.
 interface GeolocationAPI {
     
 }
-
 // geolocation.getCurretPosition(successFn);
 // geolocation.getCurrentPosition(successFn, errorFn);
 // geolocation.getCurrentPosition(successFn, errorFn, optionsObj);
