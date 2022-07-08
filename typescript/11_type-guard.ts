c89b74d5a092e050f469352690c4ac7b0ea4b19e
bokyungK <freebob2b@gmail.com>
2022-07-09 00:00:10 +0900

    Learn why we use Type Guard

---
+++ typescript/11_type-guard.ts
@@ -0,0 +1,25 @@
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function introduce (): Developer | Person{
+    return { name: 'Tony', age: 33, skill: 'Iron Making' }
+}
+const tony = introduce();
+console.log(tony.skill); // error from Union
+console.log(tony.name);
+
+if ((tony as Developer).skill) {
+    const skill = (tony as Developer).skill;
+    console.log(skill);
+} else if ((tony as Person).age) {
+    const age = (tony as Person).age;
+    console.log(age);
+}
+// 이 경우에 타입 단언으로 작성하면 코드 복잡성이 높아짐 => 타입 가드 사용
