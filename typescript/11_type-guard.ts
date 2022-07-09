3990da9d28f024e8388ee2857338b36d0d8a3966
bokyungK <freebob2b@gmail.com>
2022-07-09 23:18:51 +0900

    learn Type Guard

--- typescript/11_type-guard.ts
+++ typescript/11_type-guard.ts
@@ -23,3 +23,17 @@ if ((tony as Developer).skill) {
     console.log(age);
 }
 // 이 경우에 타입 단언으로 작성하면 코드 복잡성이 높아짐 => 타입 가드 사용
+
+
+// 타입 가드 정의
+function isDeveloper(target: Developer | Person): target is Developer {
+    return (target as Developer).skill !== undefined;
+
+}
+
+if (isDeveloper(tony)) {
+    tony.skill
+} else {
+    tony.age
+}
+// => 타입 단언으로 작성한 것에 비해 아주 간단해짐
