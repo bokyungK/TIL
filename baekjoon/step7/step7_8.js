797215668ec9936d0ede2758cd37121bd1161d73
freebob2b <freebob2b@gmail.com>
2022-09-18 21:32:11 +0900

    basic math : 10757

--- baekjoon/step7/step7_8.js
+++ baekjoon/step7/step7_8.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 const input= fs.readFileSync('/dev/stdin').toString().trim().split(' ');
-let result;
 
 input.forEach((item, idx) => {
     input[idx] = BigInt(input[idx]);
 })
 
-result = String(input[0] + input[1]);
+const result = String(input[0] + input[1]);
 console.log(result);
